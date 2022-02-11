df0681b81b416ef0e1c7b4df22ded92c04b001f1
Taylor Thomas <stth223@uky.edu>
2022-02-11 18:15:22 -0500

    Fixed bugs

--- src/CardMatch.java
+++ src/CardMatch.java
@@ -10,26 +10,56 @@ public class CardMatch extends JFrame
     private ArrayList<String> cardNames =  new ArrayList<String>() ; // arraylist of file names of card_icons
     private JPanel cardLayout = new JPanel();    // where all the cards will be placed
     private ArrayList<JButton> buttons = new ArrayList<JButton>();
+    private boolean[] buttonsMatched = new boolean[52]; // array to see if certain button is matched
     private ImageIcon backOfCard = new ImageIcon("cardIcons/back_of_card.png");
     private boolean firstLoad = true;
+    private boolean msgShown = false;
 
+    // initialize timers
+    private Timer elapsedTimer;
+    private Timer flipTimer;
 
     // buttons for user
     private JButton resetButton;
-    private JButton shuffleButton;
     private JButton giveUpButton;
     private JButton quitButton;
+
     // JPanel for buttons
     private JPanel buttonLayout;
+    // JPanel for messages
+    private JPanel messageLayout = new JPanel();
 
     // text for displaying score
-    private JLabel score;
+    private JLabel score = new JLabel("Score: 0");
+    // number of GUESSES?
+    private JLabel showGuesses = new JLabel("Guesses: 0");
+    // display time elapsed
+    private JLabel timeElapsed = new JLabel("Time Elapsed: 0");
+    // End Game Message
+    private JLabel message = new JLabel(" ");
 
+    private int time = 0;
     private int matches = 0;
+    private int guesses = 0;
     private int lastGuess = -1;
+    private int currGuess = -1;
 
     public void setCards()
     {
+        time = 0;
+        matches = 0;
+        lastGuess = -1;
+        currGuess = -1;
+        elapsedTimer.start();
+        message.setText("");
+        score.setText("Score: " + matches);
+        guesses = 0;
+        showGuesses.setText("Guesses: " + guesses);
+
+        removeMatches();
+        enableButtons();
+        repaint();
+        revalidate();
         // iterate through entire deck and create button and icon components
         // then add to the JPanel holding cards (cardLayout)
         int whichSuit = 0;
@@ -42,18 +72,24 @@ public class CardMatch extends JFrame
                 // change index in accordance to suit
                 if (i == 13 || i == 26 || i == 39){
                     whichSuit++;
-                    System.out.println(cardSuits[whichSuit]);
                 }
-
+                // initialize cardNames and buttons Mathced
                 cardNames.add("cardIcons/" + (i%13) + "_of_" + cardSuits[whichSuit] + "_icon.png");
+                buttonsMatched[i-1] = false;
+                displayCards();
             }
             // add default icon to buttons
             buttons.get(i-1).setIcon(backOfCard);
         }
         firstLoad = false;
-        displayCards();
-    }
+        shuffle();
 
+    }
+    private void removeMatches(){
+        for (int i = 0 ; i < buttonsMatched.length ; i++){
+            buttonsMatched[i] = false;
+        }
+    }
     public void displayCards(){
         cardLayout.removeAll();
         for (int i = 0 ; i < buttons.size() ; i++){
@@ -69,13 +105,36 @@ public class CardMatch extends JFrame
 
     public void displayCard(int index){
         buttons.get(index).setIcon(new ImageIcon(cardNames.get(index)));
-//        buttons.get(index).getIcon().setDescription(cardNames.get(index));
-        displayCards();
+        repaint();
     }
 
     public void hideCard(int index){
-        buttons.get(index).setIcon(backOfCard);
-        displayCards();
+        if (index >= 0) {
+            buttons.get(index).setIcon(backOfCard);
+            repaint();
+        }
+    }
+
+    public void disableButtons(int index1, int index2){
+        for (int i = 0 ; i < buttons.size() ; i++) {
+            if (i != index1 && index2 != i && !buttonsMatched[i]){
+                if (index1 == -1){
+                    buttons.get(i).setDisabledIcon(new ImageIcon(cardNames.get(i)));
+                }
+                else {
+                    buttons.get(i).setDisabledIcon(backOfCard);
+                }
+                buttons.get(i).setEnabled(false);
+            }
+        }
+    }
+
+    public void enableButtons(){
+        for (int i = 0 ; i < buttons.size() ; i++) {
+            if (!buttonsMatched[i]) {
+                buttons.get(i).setEnabled(true);
+            }
+        }
     }
 
     // method to shuffle cards into random order
@@ -95,32 +154,32 @@ public class CardMatch extends JFrame
 
     public CardMatch() {
         super("Cards");
+
+        setUpTimers();
         quitButton = new JButton("Quit");
         resetButton = new JButton("Reset");
-        shuffleButton = new JButton("Shuffle");
         giveUpButton = new JButton("Give up");
 
         // add event listeners
         quitButton.addActionListener(this);
         resetButton.addActionListener(this);
-        shuffleButton.addActionListener(this);
         giveUpButton.addActionListener(this);
 
         // add buttons to button JPanel;
         buttonLayout = new JPanel();
         buttonLayout.add(resetButton);
-        buttonLayout.add(shuffleButton);
         buttonLayout.add(quitButton);
         buttonLayout.add(giveUpButton);
 
         // add score to JPanel
-        score = new JLabel();
-        score.setText("Score: " + matches);
+
         buttonLayout.add(score);
+        buttonLayout.add(showGuesses);
+        buttonLayout.add(timeElapsed);
 
         cardLayout.setLayout( new GridLayout( 4, 13, 0, 5 ) );
 
-        // Sort cards into natural order
+        // start from beginning
         setCards();
 
         // get the content pane, onto which everything is eventually added
@@ -128,7 +187,6 @@ public class CardMatch extends JFrame
         // Add panels to the container
         c.add(cardLayout, BorderLayout.NORTH);
         c.add(buttonLayout);
-
 
         setSize(1000, 1000);
         setVisible(true);
@@ -141,47 +199,108 @@ public class CardMatch extends JFrame
         }
         else if (e.getSource() == quitButton){ // quit
             System.exit(0);
+
         }
-        else if (e.getSource() == shuffleButton) { // shuffle cards
-            shuffle();
+        else if (e.getSource() == giveUpButton){ // give up
+            gameOver();
         }
-        else if (e.getSource() == giveUpButton){
-            showAllCards();
-        }
-        else {
+        else { // a card is being clicked
             // iterate through button ArrayList to see which button was clicked
             for (int i = 0 ; i < buttons.size() ; i++){
                 if (e.getSource() == buttons.get(i) && i != lastGuess){
-                    // show card
-                    displayCard(i);
 
+                    showGuesses.setText("Guesses: " + guesses);
+                    displayCard(i);// show card
                     if (lastGuess != -1 ){ // check to see if there is a previous guess in queue
+                        guesses += 1;
+                        showGuesses.setText("Guesses: " + guesses);
                             // check chars at 10th and 11th position (1 = ace, 02 = 2, ... 13=king )
                         if (buttons.get(lastGuess).getIcon().toString().charAt(10) == buttons.get(i).getIcon().toString().charAt(10)
                             && buttons.get(lastGuess).getIcon().toString().charAt(11) == buttons.get(i).getIcon().toString().charAt(11)) {
-                                System.out.println("Match!");
-                                matches += 1;
-                                // designate matches by not allowing clicks anymore!
-                                buttons.get(i).setEnabled(false);
-                                buttons.get(lastGuess).setEnabled(false);
-                                score.setText("Score: " + matches);
+                                correctMatch(i);
 
                         }
                         else{ // If they don't share the same chars, they're not a match
                             System.out.println("Not a Match!");
-                            // hide both cards
-                            hideCard(i);
-                            hideCard(lastGuess);
+                            disableButtons(i, lastGuess);
+                            currGuess = i;
+                            flipTimer.start();
                         }
-                        lastGuess = -1;
+
                     }
                     else{ // if there is no previous guess in queue, add this guess to queue
                         lastGuess = i;
-                        System.out.println("No previous guesses. Last guess now: " + lastGuess);
                     }
                 }
             }
         }
+    }
+    private void setUpTimers(){
+        // timer object for counting how many seconds elapsed
+        elapsedTimer = new Timer(1000, new ActionListener(){
+            public void actionPerformed(ActionEvent e)
+            {
+                time += 1;
+                timeElapsed.setText("Time Elapsed: " + time);
+                repaint();
+            }
+        }
+        );
+        // timer to handle flipping of card
+        flipTimer = new Timer(2000, new ActionListener(){
+            public void actionPerformed(ActionEvent e)
+            {
+                // hide both cards
+                hideCard(lastGuess);
+                hideCard(currGuess);
+                flipTimer.stop();
+                lastGuess = -1;
+                enableButtons();
+            }
+        }
+        );
+    }
+    private void correctMatch(int i){ // method ran when a correct match is clicked
+        System.out.println("Match!");
+        matches += 1;
+        // designate matches by not allowing clicks anymore!
+        buttons.get(i).setDisabledIcon(buttons.get(i).getIcon());
+        buttons.get(i).setEnabled(false);
+        buttons.get(lastGuess).setDisabledIcon(buttons.get(lastGuess).getIcon());
+        buttons.get(lastGuess).setEnabled(false);
+        // encode which buttons have been matched
+        buttonsMatched[i] = true;
+        buttonsMatched[lastGuess] = true;
+        score.setText("Score: " + matches);
+        lastGuess = -1;
+        if (matches == 26){
+            gameOver();
+        }
+    }
+    private void gameOver(){
+        elapsedTimer.stop();
+        Container c = getContentPane();
+        messageLayout.setSize(1000, 300);
+        if (matches == 26){
+            c.remove(cardLayout);
+            message.setText("You Won! Your Time was: " + time +  "... Reset to play again.");
+            if (!msgShown) {
+                messageLayout.add(message);
+                msgShown = true;
+            }
+        }
+        else{
+            disableButtons(-1, -1);
+            showAllCards();
+            message.setText("You lost. Try again by clicking 'Reset'");
+            if (!msgShown) {
+                messageLayout.add(message);
+                msgShown = true;
+            }
+        }
+        buttonLayout.add(messageLayout, BorderLayout.NORTH);
+        repaint();
+        revalidate();
     }
 
     public static void main(String args[])
